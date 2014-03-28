a83d4d10d354ff0009e64f2420eca8c7dc65ff57
myun <monica.t.yun@gmail.com>
2014-03-28 10:36:29 -0700

    Can seed recipes table.

--- seed.py
+++ seed.py
@@ -1,10 +1,10 @@
 import model
 import csv
+import re
 
 # Seeds database with recipe data scraped from BigOven.com
 def load_recipes(session):
     with open("seed_data/recipes.csv") as f:
-        print "I am here!"
         reader = csv.reader(f)
         for row in reader:
             ingredient_names, name, orig_URL, orig_rating, num_ratings, image_URL, directions, serving_size, ingredient_amounts, categories = row
@@ -16,6 +16,23 @@ def load_recipes(session):
             print "serving size is", serving_size
             print "ingredient amounts are", ingredient_amounts
             print "categories are", categories
+            print "original rating is ", orig_rating
+
+            # # Process ingredient names. 
+            # ingredient_names = process_ingredients(ingredient_names)
+
+            # Process rating. Example of original format of rating: '4.5 avg'. Needs to rounded and converted to an integer for simplicity.
+            rating_number = orig_rating.split()[0]
+            rounded_rating = round(float(rating_number))
+            orig_rating = int(rounded_rating)
+
+            # Process number of ratings. Example of original format: '1 review(s)'. Needs to be converted to a single integer. 
+            num_ratings = int(num_ratings.split()[0])
+
+            # Convert the name, ingredient names, and directions of the recipe in case foreign characters were originally used. 
+            name = name.decode("latin-1")
+            ingredient_names = ingredient_names.decode("latin-1")
+            directions = directions.decode("latin-1")
             # load_common_ingredients(ingredient_names)
             # load_recipe_ingredients(name, ingredients)
             # load_common_categories(categories)
@@ -29,6 +46,26 @@ def load_recipes(session):
                                   directions=directions)
             model.session.add(recipe)
         model.session.commit()
+
+def process_ingredients(ingredient_names):
+    # Example of original format of each ingredient name: '<span itemprop="name" class="name">regular <a href="/glossary/all-purpose%20flour" class="glosslink">all-purpose flour</a></span>''
+    ingredient_list = []
+    for ingredient in ingredient_names:
+        # Extract only the text between tags '>' and '<'; results in a list of words that make up the full ingredient name
+        extracted_words = re.findall(r'>[a-zA-ZS0-9() _]+<', ingredient)
+        full_name = ""
+        for word in extracted_words:
+            word = word.strip(' ><')
+
+            # Add all the individual words together to form the complete ingredient name
+            full_name += word
+
+            # Add a space between each word in the name
+            full_name += " "
+
+        # Leave off the trailing space after the last word when appending to the final ingredient list
+        ingredient_list.append(full_name[:-1])
+    return ingredient_list
 
 def load_common_ingredients(session, ingredient_names):
     common_ingredients = []
@@ -50,17 +87,6 @@ def load_recipe_ingredients(session, recipename, ingredients):
                                                    common_ingredient_id=ingredient_id)
         session.add(recipe_ingredient)
     session.commit()
-
-
-
-    # name = Column(String(150), nullable=False)
-    # orig_site = Column(String(150), nullable=False)
-    # orig_URL = Column(String(240), nullable=False)
-    # orig_rating = Column(Integer, nullable = True)
-    # num_ratings = Column(Integer, nullable = True)
-    # image_URL = Column(String(240), nullable=True)
-    # serving_size = Column(String(100), nullable=True)
-    # directions = Column(String(12000000), nullable=False)
 
 # def load_users(session):
 #     with open("seed_data/u.user") as f:
