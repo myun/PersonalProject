1878e3954e14832669190a924d1a89a98943fd7d
myun <monica.t.yun@gmail.com>
2014-03-28 13:10:19 -0700

    Can seed all tables in database with scraped data.

--- seed.py
+++ seed.py
@@ -2,29 +2,19 @@ import model
 import csv
 import re
 
+# Keep track of all ingredients and categories present in the database as a whole.
+common_ingredients = []
+common_categories = []
+
 # Seeds database with recipe data scraped from BigOven.com
-def load_recipes(session):
+def process_data(session):
     with open("seed_data/recipes.csv") as f:
         reader = csv.reader(f)
         for row in reader:
             ingredient_names, name, orig_URL, orig_rating, num_ratings, image_URL, directions, serving_size, ingredient_amounts, categories = row
-            print "Ingredient names are:", ingredient_names
-            print "Recipe name is", name
-            print "Original URL is", orig_URL
-            print "Image URL is", image_URL
-            print "directions are", directions
-            print "serving size is", serving_size
-            print "ingredient amounts are", ingredient_amounts
-            print "categories are", categories
-            print "original rating is ", orig_rating
 
-            # # Process ingredient names. 
-            # ingredient_names = process_ingredients(ingredient_names)
-
-            # Process rating. Example of original format of rating: '4.5 avg'. Needs to rounded and converted to an integer for simplicity.
-            rating_number = orig_rating.split()[0]
-            rounded_rating = round(float(rating_number))
-            orig_rating = int(rounded_rating)
+            # Process rating. 
+            orig_rating = process_rating(orig_rating)
 
             # Process number of ratings. Example of original format: '1 review(s)'. Needs to be converted to a single integer. 
             num_ratings = int(num_ratings.split()[0])
@@ -33,27 +23,52 @@ def load_recipes(session):
             name = name.decode("latin-1")
             ingredient_names = ingredient_names.decode("latin-1")
             directions = directions.decode("latin-1")
-            # load_common_ingredients(ingredient_names)
-            # load_recipe_ingredients(name, ingredients)
-            # load_common_categories(categories)
-            recipe = model.Recipe(name=name,
-                                  orig_site="Big Oven",
-                                  orig_URL=orig_URL,
-                                  orig_rating=orig_rating,
-                                  num_ratings=num_ratings,
-                                  image_URL=image_URL,
-                                  serving_size=serving_size,
-                                  directions=directions)
-            model.session.add(recipe)
-        model.session.commit()
+
+            # Process ingredient names to remove all remaining html tags and get pure text. 
+            ingredient_names = process_ingredients(ingredient_names)
+
+            # Format ingredient amounts from string to list of strings.
+            ingredient_amounts = process_ingredient_amounts(ingredient_amounts)
+
+            # Format categories from string to list of strings.
+            categories = process_categories(categories)
+
+            load_recipes(session, name, orig_URL, orig_rating, num_ratings, image_URL, serving_size, directions)
+            load_common_ingredients(session, ingredient_names)
+            load_recipe_ingredients(session, name, ingredient_names, ingredient_amounts)
+            load_common_categories(session, categories)
+            load_recipes_categories(session, name, categories)
+
+def load_recipes(session, name, orig_URL, orig_rating, num_ratings, image_URL, serving_size, directions):
+    recipe = model.Recipe(name=name,
+                          orig_site="Big Oven",
+                          orig_URL=orig_URL,
+                          orig_rating=orig_rating,
+                          num_ratings=num_ratings,
+                          image_URL=image_URL,
+                          serving_size=serving_size,
+                          directions=directions)
+    model.session.add(recipe)
+    model.session.commit()
+
+# Example of original format of rating: '4.5 avg'. Needs to rounded and converted to an integer for simplicity.
+def process_rating(orig_rating):
+    rating_number = orig_rating.split()[0]
+    rounded_rating = round(float(rating_number))
+    orig_rating = int(rounded_rating)
+    return orig_rating
 
 def process_ingredients(ingredient_names):
     # Example of original format of each ingredient name: '<span itemprop="name" class="name">regular <a href="/glossary/all-purpose%20flour" class="glosslink">all-purpose flour</a></span>''
-    ingredient_list = []
-    for ingredient in ingredient_names:
+    formatted_ingredients = []
+    
+    unformatted_ingredients = ingredient_names.split(",")
+
+    for ingredient in unformatted_ingredients:
         # Extract only the text between tags '>' and '<'; results in a list of words that make up the full ingredient name
         extracted_words = re.findall(r'>[a-zA-ZS0-9() _]+<', ingredient)
         full_name = ""
+        
         for word in extracted_words:
             word = word.strip(' ><')
 
@@ -64,11 +79,23 @@ def process_ingredients(ingredient_names):
             full_name += " "
 
         # Leave off the trailing space after the last word when appending to the final ingredient list
-        ingredient_list.append(full_name[:-1])
-    return ingredient_list
+        formatted_ingredients.append(full_name[:-1].lower())
+    return formatted_ingredients
+
+def process_ingredient_amounts(ingredient_amounts):
+    ingredient_amount_list = ingredient_amounts.split(",")
+    for ingredient_amount in ingredient_amount_list:
+        ingredient_amount = ingredient_amount.strip()
+    return ingredient_amount_list
+
+def process_categories(categories):
+    category_list = categories.split(",")
+    for category in category_list:
+        category = category.strip()
+    return category_list
 
 def load_common_ingredients(session, ingredient_names):
-    common_ingredients = []
+    global common_ingredients
     for ingredient in ingredient_names:
         if ingredient not in common_ingredients:
             common_ingredients.append(ingredient)
@@ -76,36 +103,53 @@ def load_common_ingredients(session, ingredient_names):
             session.add(new_ingredient)
     session.commit()
 
-def load_recipe_ingredients(session, recipename, ingredients):
+def load_recipe_ingredients(session, recipename, ingredient_names, ingredient_amounts):
     recipe = session.query(model.Recipe).filter_by(name=recipename).first()
     recipe_id = recipe.id
-    for ingredient in ingredients:
+    for i in range(0, len(ingredient_names)):
+        ingredient = ingredient_names[i]
         ingredient_id = session.query(model.CommonIngredient).filter_by(name=ingredient).first().id
-        ingredient_amount = ingredients[ingredient]
-        recipe_ingredient = model.RecipeIngredient(amount=ingredient_amount,
-                                                   recipe_id=recipe_id,
-                                                   common_ingredient_id=ingredient_id)
-        session.add(recipe_ingredient)
+        ingredient_amount = ingredient_amounts[i]
+        if ingredient_amount != " ":
+
+            # # Standardize spelling of 'tbsp' vs. 'tablespoon', 'tsp' vs. 'teaspoon', etc if they are present.
+            # amount_sep= ingredient_amount.split()
+            # if len(amount_sep) > 1:
+            #     measurement = amount_sep[1]
+            #     if measurement = "tbsp":
+            #         measurement = "tablespoon"
+            #     elif measurement = "c":
+            #         measurement = "cup(s)"
+
+            recipe_ingredient = model.RecipeIngredient(amount=ingredient_amount,
+                                                       recipe_id=recipe_id,
+                                                       common_ingredient_id=ingredient_id)
+            session.add(recipe_ingredient)
+        session.commit()
+
+def load_common_categories(session, categories):
+    global common_categories
+    for category in categories:
+        category = category.lower()
+        if category not in common_categories:
+            common_categories.append(category)
+            new_category = model.CommonCategory(name=category)
+            session.add(new_category)
     session.commit()
 
-# def load_users(session):
-#     with open("seed_data/u.user") as f:
-#         reader = csv.reader(f, delimiter="|")
-#         for row in reader:
-#             id, age, gender, occupation, zipcode = row
-#             id = int(id)
-#             age = int(age)
-#             u = model.User(id=id,
-#                            email=None,
-#                            password=None,
-#                            age=age,
-#                            zipcode=zipcode)
-#             session.add(u)
-#         session.commit()
+def load_recipes_categories(session, recipe_name, categories):
+    recipe = session.query(model.Recipe).filter_by(name=recipe_name).first()
+    recipe_id = recipe.id
+
+    for category in categories:
+        category = category.lower()
+        category_id = session.query(model.CommonCategory).filter_by(name=category).first().id
+        recipe_category = model.RecipeCategory(recipe_id=recipe_id, common_category_id=category_id)
+        session.add(recipe_category)
+    session.commit()
 
 def main(session):
-    load_recipes(model.session)
-    # load_common_ingredients(session)
+    process_data(model.session)
 
 if __name__ == "__main__":
 #     s=model.connect()
